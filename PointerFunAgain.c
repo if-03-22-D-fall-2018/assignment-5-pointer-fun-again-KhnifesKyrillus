9387c4acbae4dce9b0b026a585fc557387ac4f05
Kyrillus Khnifes <khnifes.kyrillus@gmail.com>
2018-10-24 21:39:24 +0200

    prin_struct works fine

--- PointerFunAgain.c
+++ PointerFunAgain.c
@@ -1,27 +1,30 @@
 #include "stdio.h"
-
-
-
+#include "string.h"
 
 struct PlayStruct
 {
   int int_value;
-  double double_value
+  double double_value;
   char a_string[64];
 };
+void print_struct(struct PlayStruct ps, struct PlayStruct* pps);
 
 int main(int argc, char const *argv[])
 {
   struct PlayStruct play_struct;
-  struct* PlayStruct play_struct_pointer;
-
-  play_struct.int_value=123;
-  play_struct.double_value=1.23;
-  play_struct.a_string="Hallo";
-  play_struct_pointer=&play_struct;
+  printf("Enter a string: ");
+  scanf("%s",play_struct.a_string );
+  printf("\nEnter an Integer: ");
+  scanf("%d",&play_struct.int_value );
+  printf("\nEnter a Double: ");
+  scanf("%lf",&play_struct.double_value );
+  printf("\n");
+  struct PlayStruct* play_struct_pointer=&play_struct;
+  print_struct(play_struct,play_struct_pointer);
   return 0;
 }
-void print_struct(struct PlayStruct ps, struct* PlayStruct pps)
+void print_struct(struct PlayStruct ps, struct PlayStruct* pps)
 {
-  printf("%s %d %d\n",ps.a_string,ps.double_value,ps.int_value );
+  printf("ps:\nString: %s\nDouble: %f\nInt: %d\n",ps.a_string,ps.double_value,ps.int_value );
+  printf("\npps:\nString: %s\nDouble: %.3lf\nInt: %d\n",pps->a_string,pps->double_value,pps->int_value);
 }
